efefbc7acb2123511421cbdf351f50bc3507f785
Nicolas Wolf <me@nicolaswolf.io>
2022-03-14 18:24:16 +0100

    add stream test

---
+++ stream_test.go
@@ -0,0 +1,27 @@
+package gosse
+
+import (
+	"context"
+	"github.com/imkira/go-observer"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStream_WaitNextCtx(t *testing.T) {
+	property := observer.NewProperty(nil)
+	stream := newStream(property.Observe())
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	property.Update("test")
+
+	next, err := stream.WaitNextCtx(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, "test", next)
+
+	cancel()
+	next, err = stream.WaitNextCtx(ctx)
+	assert.Error(t, err)
+	assert.Equal(t, ctx.Err(), err)
+	assert.Nil(t, next)
+}
